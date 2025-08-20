5e5f63cc590cc265b6832e50487f814954a40925
pthengane <pratik.thengane@gmail.com>
2025-08-20 19:00:13 +0530

    first commit

---
+++ src/ClassDemo.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+class ClassDemo extends Component
+{
+    constructor()
+    {
+        super();
+    }
+    render()
+    {
+        return(
+            <div>
+                <h2> Good Morning From Pratik</h2>
+                <h3>I am 23 Years old. Currently, I am learning Reactjs at Hefshine.</h3>
+            </div>
+        )
+    }
+}
+export default ClassDemo;
