ec372f34916fae7174d53c4fca347044ac3ff26f
pthengane <pratik.thengane@gmail.com>
2025-08-24 20:17:24 +0530

    ClassComp Commit

---
+++ src/ClassComp.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+class ClassComp extends Component
+{
+    constructor()
+    {
+        super();
+    }
+    render()
+    {
+        return(
+            <div>
+                <h1>Welcome to class component</h1>
+            </div>
+        )
+    }
+}
+export default ClassComp;
