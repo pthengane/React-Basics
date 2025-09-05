fd6c33082731a4d4b8915f9e40b7cef42ebc7928
Pratik Thengane <131654944+pthengane@users.noreply.github.com>
2025-09-05 11:53:35 +0530

    GridDemo Commit

---
+++ src/Griddemo.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Griddemo() {
+  return (
+    <div className="container">
+        
+     <div className="row">   
+        <div className="col-4">col 1</div>
+        <div className="col-6">col 2</div>
+        <div className="col-2">col 3</div>
+
+        </div>
+
+    <div className="row">   
+        <div className="col-1">col 1,1</div>
+        <div className="col-3">col 1,2</div>
+        <div className="col-5">col 1,3</div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+
+ 
